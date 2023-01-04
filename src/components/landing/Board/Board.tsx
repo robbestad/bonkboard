58e4177e7812c55df2b53d542f2a3f87bdac4c8c
FP Lee <97802371+fplord@users.noreply.github.com>
2023-01-04 11:38:43 +0000

    stuff

--- src/components/landing/Board/Board.tsx
+++ src/components/landing/Board/Board.tsx
@@ -10,9 +10,37 @@ export function Board() {
       const context = canvas.getContext("2d");
       if (context) {
         // draw something on canvas on load
+        // TODO get attr from Box, no magic numbers
+        const imageData = context.createImageData(500, 500);
+        const {data} = imageData;
+        for (let i = 0; i < data.length; i += 4) {
+          data[i] = 255; // red
+          data[i + 1] = 0; // green
+          data[i + 2] = 0; // blue
+          data[i + 3] = 255; // alpha channel
+        }
+        context.putImageData(imageData, 0, 0);
       }
     }
   }, []);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      // const canvas = canvasRef.current;
+      // canvas.addEventListener("mousemove", (event) => pick(event, hoveredColor));
+      // canvas.addEventListener("click", (event) => pick(event, selectedColor));
+    }
+  });
+
+  /*
+  function pick(event, destination) {
+    const bounding = canvas.getBoundingClientRect();
+    const x = event.clientX - bounding.left;
+    const y = event.clientY - bounding.top;
+    const pixel = ctx.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+  }
+  */
 
   return (
     <Flex direction="column" align="center" justify="center">
