b1690f003df060acc91bf59d40d230f906183182
Johnnycus <Johnnycus@users.noreply.github.com>
2023-01-04 17:12:30 +0600

    Init canvas again?

--- src/components/landing/Board/Board.tsx
+++ src/components/landing/Board/Board.tsx
@@ -1,10 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export function Board() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+      if (context) {
+        // draw something on canvas on load
+      }
+    }
+  }, []);
+
   return (
-    <Box>
+    <Flex direction="column" align="center" justify="center">
       Wow such board! gg
-      <Box as="canvas" width="500" height="500" border="1px" />
-    </Box>
+      <Box
+        // @ts-ignore
+        ref={canvasRef}
+        as="canvas"
+        width="500px"
+        height="500px"
+        border="1px"
+        mt={8}
+      />
+    </Flex>
   );
 }
