e24219a78108eb57f44160dab8310b40ad908d49
rczjian <rczjian@gmail.com>
2023-01-04 23:10:51 +0800

    implement zoom and paint functions

--- src/components/landing/Board/Board.tsx
+++ src/components/landing/Board/Board.tsx
@@ -1,30 +1,36 @@
 import { useEffect, useRef, useState } from "react";
 import { RgbaColor, RgbaColorPicker } from "react-colorful";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 const CANVAS_SIZE = {
   width: 500,
   height: 500,
 };
 
+const SCALE = 3;
+
 export function Board() {
   const [color, setColor] = useState<RgbaColor>({ r: 0, g: 0, b: 0, a: 1 });
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const zoomCanvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
+    const canvas = canvasRef.current;
+    if (canvas) {
       const context = canvas.getContext("2d");
       if (context) {
         // draw something on canvas on load
         // TODO get attr from Box, no magic numbers
-        const imageData = context.createImageData(500, 500);
-        const {data} = imageData;
+        const imageData = context.createImageData(
+          CANVAS_SIZE.width,
+          CANVAS_SIZE.height
+        );
+        const { data } = imageData;
         for (let i = 0; i < data.length; i += 4) {
           data[i] = 255; // red
-          data[i + 1] = 0; // green
-          data[i + 2] = 0; // blue
+          data[i + 1] = 255; // green
+          data[i + 2] = 255; // blue
           data[i + 3] = 255; // alpha channel
         }
         context.putImageData(imageData, 0, 0);
@@ -32,23 +38,54 @@ export function Board() {
     }
   }, []);
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      // const canvas = canvasRef.current;
-      // canvas.addEventListener("mousemove", (event) => pick(event, hoveredColor));
-      // canvas.addEventListener("click", (event) => pick(event, selectedColor));
+  function showZoom(e: any) {
+    const canvas = canvasRef.current;
+    const zoomCanvas = zoomCanvasRef.current;
+    const [x, y] = getCursorPosition(e);
+    if (canvas && zoomCanvas) {
+      const zoomContext = zoomCanvas.getContext("2d");
+      if (zoomContext) {
+        zoomContext.imageSmoothingEnabled = false;
+        zoomContext.drawImage(
+          canvas,
+          Math.min(Math.max(0, x - 5), CANVAS_SIZE.width - 10),
+          Math.min(Math.max(0, y - 5), CANVAS_SIZE.height - 10),
+          11,
+          11,
+          0,
+          0,
+          500,
+          500
+        );
+        zoomContext.strokeStyle = "yellow";
+        zoomContext.lineWidth = 3;
+        zoomContext.strokeRect(250 - 23, 250 - 23, 46, 46);
+      }
     }
-  });
-
-  /*
-  function pick(event, destination) {
-    const bounding = canvas.getBoundingClientRect();
-    const x = event.clientX - bounding.left;
-    const y = event.clientY - bounding.top;
-    const pixel = ctx.getImageData(x, y, 1, 1);
-    const data = pixel.data;
   }
-  */
+
+  function paint(e: any) {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        const [x, y] = getCursorPosition(e);
+        context.fillStyle = "black";
+        context?.fillRect(x, y, 1, 1);
+      }
+    }
+  }
+
+  function getCursorPosition(event: any) {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const rect = canvas.getBoundingClientRect();
+      const x = Math.floor((event.clientX - rect.left) / SCALE);
+      const y = Math.floor((event.clientY - rect.top) / SCALE);
+      return [x, y];
+    }
+    return [-1, -1];
+  }
 
   return (
     <Flex direction="column" align="center" justify="center" gap={8}>
@@ -56,14 +93,41 @@ export function Board() {
 
       <RgbaColorPicker color={color} onChange={setColor} />
 
-      <Box
-        // @ts-ignore
-        ref={canvasRef}
-        as="canvas"
-        width={`${CANVAS_SIZE.width}px`}
-        height={`${CANVAS_SIZE.height}px`}
-        border="1px"
-      />
+      <Flex direction="row">
+        <div>
+          <canvas
+            // @ts-ignore
+            ref={canvasRef}
+            width={CANVAS_SIZE.width}
+            height={CANVAS_SIZE.height}
+            style={{
+              imageRendering: "pixelated",
+              border: "1px solid black",
+              transform: `scale(${SCALE})`,
+            }}
+            onMouseMove={(e) => {
+              showZoom(e);
+            }}
+            onClick={(e) => {
+              paint(e);
+              showZoom(e);
+            }}
+          />
+        </div>
+        <div>
+          <canvas
+            // @ts-ignore
+            ref={zoomCanvasRef}
+            width={CANVAS_SIZE.width}
+            height={CANVAS_SIZE.height}
+            style={{
+              imageRendering: "pixelated",
+              border: "1px solid black",
+              transform: "scale(1)",
+            }}
+          />
+        </div>
+      </Flex>
     </Flex>
   );
 }
